7f8edcc825bf3be3612b6421521a30196cd2c5d4
jfaynel <99059230+jfaynel@users.noreply.github.com>
2022-04-30 23:33:07 -0500

    Create main_2.js

---
+++ weekly_mission_1/ejercicio_8/package/main_2.js
@@ -0,0 +1,8 @@
+// name clash
+import { log } from './logger.js'
+// const log = console.log // <- this would generate a "SyntaxError: Identifier 'log' has already been declared" error
+log('Hello world')
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
